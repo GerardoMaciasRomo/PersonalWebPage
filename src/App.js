33ea0e0a919555c309d17b7a804997e846563de4
GerardoMaciasRomo <117399248+GerardoMaciasRomo@users.noreply.github.com>
2025-09-25 11:50:49 -0600

    router problem fixed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React, { useState, useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Hobbies from "./pages/Hobbies";
 import Professional from "./pages/Profesional";
 import Academic from "./pages/Academic";
 import ScrollToTop from "./components/ScrollToTop";
-
 function App() {
   return (
-    <Routes>
-      <ScrollToTop />
-      <Route path="/" element={<Home />} />
-      <Route path="/hobbies" element={<Hobbies />} />
-      <Route path="/professional" element={<Professional />} />
-      <Route path="/academic" element={<Academic />} />
-    </Routes>
+ <>
+  <ScrollToTop />
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/hobbies" element={<Hobbies />} />
+    <Route path="/professional" element={<Professional />} />
+    <Route path="/academic" element={<Academic />} />
+  </Routes>
+</>
+    
   );
 }
 
 export default App;
-
